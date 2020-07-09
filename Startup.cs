fddd35e5463539f58e837c835cce688c8f872a06
Trent Richardson <trentrichardson@pop-os.localdomain>
2020-07-09 10:11:40 +0200

    + add swagger

--- Startup.cs
+++ Startup.cs
@@ -21,6 +21,7 @@ namespace SelfCatering
             services.AddControllers();
 
             services.AddSingleton<IReservationStore, DemoInMemoryReservationStore>();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +35,16 @@ namespace SelfCatering
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Self Catering V1");
+            });
 
             app.UseAuthorization();
 
