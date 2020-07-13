ba5b812e259752d0ac06a29c44a6b21819cf95b8
Trent Richardson <trentrichardson@pop-os.localdomain>
2020-07-13 08:27:04 +0200

    + add identity server setup

---
+++ auth/src/Startup.cs
@@ -0,0 +1,61 @@
+﻿using IdentityServer4.Services;
+using IdentityServerHost.Quickstart.UI;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace AuthDemo
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityServer(options =>
+            {
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+            })
+                .AddInMemoryApiScopes(Config.GetApiScopes())
+                .AddInMemoryApiResources(Config.GetApis())
+                .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                .AddInMemoryClients(Config.GetClients())
+                .AddTestUsers(TestUsers.Users)
+                .AddDeveloperSigningCredential(persistKey: false);
+
+            services.AddAuthentication();
+            services.AddAuthorization();
+
+            // add CORS policy for non-IdentityServer endpoints
+            services.AddCors(options =>
+            {
+                options.AddPolicy("api", policy =>
+                {
+                    policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                });
+            });
+
+            // demo versions (never use in production)
+            services.AddTransient<ICorsPolicyService, DemoCorsPolicy>();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseSerilogRequestLogging();
+
+            app.UseCors("api");
+
+            app.UseIdentityServer();
+            app.UseAuthorization();
+        }
+    }
+}
